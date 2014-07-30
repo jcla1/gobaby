0956db6a45f4d6dc0e479508cd79a7b83ec9f485
Joseph Adams <whitegolem@gmail.com>
2014-07-30 14:03:34 +0100

    Added the rest of the rel small instr set.

--- executor.go
+++ executor.go
@@ -19,15 +19,27 @@ func (b *Baby) Step() {
     fmt.Printf("%02d ", b.CurrentInstruction)
 
     switch instr := b.Memory[b.CurrentInstruction]; instr & 0x0000F000 {
-    case 0: // JMP
-        fmt.Printf("JMP %d\n", instr & 0x0000000F)
-    case 16384: // LDN
-        fmt.Printf("LDN %d\n", instr & 0x0000000F)
-    case 24576: // STO
-        fmt.Printf("STO %d\n", instr & 0x0000000F)
-    case 32768: // SUB
-        fmt.Printf("SUB %d\n", instr & 0x0000000F)
+    case 0x00000000: // JMP
+        fmt.Printf("JMP %d\n", instr & 0x0000001F)
+    case 0x00004000: // LDN
+        fmt.Printf("LDN %d\n", instr & 0x0000001F)
+    case 0x00006000: // STO
+        fmt.Printf("STO %d\n", instr & 0x0000001F)
+    case 0x00008000: // SUB
+        fmt.Printf("SUB %d\n", instr & 0x0000001F)
+    case 0x0000C000: // CMP
+        fmt.Printf("CMP\n")
+    case 0x00002000: // CMP
+        fmt.Printf("JRP %d\n", instr & 0x0000001F)
+    case 0x0000E000: // STP
+        fmt.Printf("STP\n")
     default:
         fmt.Printf("unknown instr: %d\n", instr & 0x0000F000)
+    }
+}
+
+func (b *Baby) Run() {
+    for (b.Memory[b.CurrentInstruction] & 0x0000F000) != 0x0000E000 {
+        b.Step()
     }
 }
