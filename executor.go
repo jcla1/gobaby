e529045ebf98413280de6de158f59a3beda84e8b
Joseph Adams <whitegolem@gmail.com>
2014-07-30 12:54:07 +0100

    Using step function to switch on different instructions.

--- executor.go
+++ executor.go
@@ -1,5 +1,9 @@
 package baby
 
+import (
+   "fmt"
+)
+
 type Baby struct {
     CurrentInstruction int8
     Accumulator int32
@@ -7,4 +11,13 @@ type Baby struct {
     // Bits 0-4 make up an optional address, while
     // the actual instruction lives in 13-15
     Memory [32]int32
+}
+
+func (b *Baby) Step() {
+    b.CurrentInstruction++
+
+    switch instr := b.Memory[b.CurrentInstruction]; {
+    case (instr & 0x0000000F) == 0: // JMP
+        fmt.Printf("%02d JMP %d\n", b.CurrentInstruction, instr & 0x0000000F)
+    }
 }
