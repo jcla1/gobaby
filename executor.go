c1056e991d94ddba90629de9ee69bc5363c699e2
Joseph Adams <whitegolem@gmail.com>
2014-07-31 14:41:26 +0100

    Added a line to opcode converter

--- executor.go
+++ executor.go
@@ -1,11 +1,14 @@
 package baby
 
 import (
-   "fmt"
+    "regexp"
+    "strconv"
 )
 
+var lineRegex = regexp.MustCompile("[0-9]* (JMP|JRP|LDN|STO|SUB|CMP|STP) ?([0-9]+)?")
+
 type Baby struct {
-    CurrentInstruction int8
+    CurrentInstruction int32
     Accumulator int32
 
     // Bits 0-4 make up an optional address, while
@@ -45,4 +48,26 @@ func (b *Baby) Run() {
     for (b.Memory[b.CurrentInstruction] & 0x0000F000) != 0x0000E000 {
         b.Step()
     }
+}
+
+func InstrToOpCode(instr string) int32 {
+    matches := lineRegex.FindStringSubmatch(instr)[1:]
+
+    if len(matches) < 1 {
+        panic("unknown opcode!")
+    }
+
+    opCode, _ := strconv.Atoi(matches[2])
+
+    switch matches[1] {
+    case "JMP": break
+    case "JRP": opCode |= 0x00002000
+    case "LDN": opCode |= 0x00004000
+    case "STO": opCode |= 0x00006000
+    case "SUB": opCode |= 0x00008000
+    case "CMP": opCode |= 0x0000C000
+    case "STP": opCode |= 0x0000E000
+    }
+
+    return int32(opCode)
 }
