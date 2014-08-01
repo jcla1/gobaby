0d668cbc1a454fd1374c4af91878f74fe8e17ac8
Joseph Adams <whitegolem@gmail.com>
2014-08-01 09:43:14 +0100

    Fixed some little problems with the interpeter. Moved Step into Run. Everything is now unsigned.

--- executor.go
+++ executor.go
@@ -1,57 +1,57 @@
 package baby
 
 import (
+	"fmt"
 	"regexp"
 	"strconv"
 	"strings"
 )
 
-var lineRegex = regexp.MustCompile("[0-9]* *(JMP|JRP|LDN|STO|SUB|CMP|STP) *?([0-9]+)?")
+var lineRegex = regexp.MustCompile("[0-9]* *(NUM|JMP|JRP|LDN|STO|SUB|CMP|STP)( *)?(-?[0-9]+)?")
 
 type Baby struct {
-	CurrentInstruction int32
-	Accumulator        int32
+	CurrentInstruction uint32
+	Accumulator        uint32
 
 	// Bits 0-4 make up an optional address, while
 	// the actual instruction lives in 13-15
-	Memory [32]int32
-}
-
-func (b *Baby) Step() {
-	b.CurrentInstruction++
-
-	instr := b.Memory[b.CurrentInstruction]
-	data := instr & 0x0000001F
-
-	switch instr & 0x0000F000 {
-	case 0x00000000: // JMP
-		b.CurrentInstruction = b.Memory[data]
-	case 0x00002000: // JRP
-		b.CurrentInstruction += b.Memory[data]
-	case 0x00004000: // LDN
-		b.Accumulator = -b.Memory[data]
-	case 0x00006000: // STO
-		b.Memory[data] = b.Accumulator
-	case 0x00008000: // SUB
-		b.Accumulator -= b.Memory[data]
-	case 0x0000C000: // CMP
-		if b.Accumulator < 0 {
-			b.CurrentInstruction++
-		}
-	case 0x0000E000: // STP
-		return
-	default:
-		panic("trying to execute non-instruction code!")
-	}
+	Memory [32]uint32
 }
 
 func (b *Baby) Run() {
-	for (b.Memory[b.CurrentInstruction] & 0x0000F000) != 0x0000E000 {
-		b.Step()
+	for {
+		// fmt.Printf("CI : %d\nACC: %d\n-----\n", int32(b.CurrentInstruction), int32(b.Accumulator))
+		// fmt.Scanln()
+		b.CurrentInstruction++
+
+		instr := b.Memory[int32(b.CurrentInstruction)%32]
+		data := instr & 0x0000001F
+
+		switch instr & 0x0000E000 {
+		case 0x00000000: // JMP
+			b.CurrentInstruction = b.Memory[data]
+		case 0x00002000: // JRP
+			b.CurrentInstruction += b.Memory[data]
+		case 0x00004000: // LDN
+			b.Accumulator = -b.Memory[data]
+		case 0x00006000: // STO
+			b.Memory[data] = b.Accumulator
+		case 0x00008000: // SUB
+			b.Accumulator -= b.Memory[data]
+		case 0x0000C000: // CMP
+			if int32(b.Accumulator) < 0 {
+				b.CurrentInstruction++
+			}
+		case 0x0000E000: // STP
+			return
+		default:
+			fmt.Println("malicious position:", b.CurrentInstruction)
+			panic("trying to execute non-instruction code!")
+		}
 	}
 }
 
-func instrToOpCode(instr string) int32 {
+func instrToOpCode(instr string) uint32 {
 	matches := lineRegex.FindStringSubmatch(instr)[1:]
 
 	if len(matches) < 1 {
@@ -60,8 +60,8 @@ func instrToOpCode(instr string) int32 {
 
 	opCode, _ := strconv.Atoi(matches[2])
 
-	switch matches[1] {
-	case "JMP":
+	switch matches[0] {
+	case "JMP", "NUM":
 		break
 	case "JRP":
 		opCode |= 0x00002000
@@ -76,18 +76,22 @@ func instrToOpCode(instr string) int32 {
 	case "STP":
 		opCode |= 0x0000E000
 	}
-
-	return int32(opCode)
+	// fmt.Printf("%032b\n", uint32(opCode))
+	return uint32(opCode)
 }
 
-func MemoryFromString(prog string) [32]int32 {
+func MemoryFromString(prog string) [32]uint32 {
 	lines := strings.Split(prog, "\n")
 
-	var memory [32]int32
+	var memory [32]uint32
 	memoryIndex := 0
 
 	for i := 0; i < len(lines) && memoryIndex < len(memory); i++ {
 		line := strings.TrimSpace(lines[i])
+		if line == "" {
+			continue
+		}
+
 		if !strings.HasPrefix(line, ";") {
 			memory[memoryIndex] = instrToOpCode(line)
 			memoryIndex++
